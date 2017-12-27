202e7d1848213fd9d0ebdee9666709c1acd2fedf
L-Cafe <L-Cafe@users.noreply.github.com>
2017-12-28 07:30:08 +0900

    lol

--- launch.bash
+++ launch.bash
@@ -1,9 +1,9 @@
-#! /usr/bin env bash
+#!/usr/bin env bash
 
 # Telegram Bot token
-export telegramToken="<YOUR BOT TOKEN HERE>"
+export telegram_token="<YOUR BOT TOKEN HERE>"
 # Telegram admin ID
-export telegramAdminId="<YOUR ID HERE>"
+export telegram_admin_id="<YOUR ID HERE>"
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
