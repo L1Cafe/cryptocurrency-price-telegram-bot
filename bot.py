4ed161d6ffeb318d69dd786e44fcc43b64e48398
L-Cafe <L-Cafe@users.noreply.github.com>
2017-12-28 18:00:58 +0900

    ok

--- bot.py
+++ bot.py
@@ -17,20 +17,19 @@ start_handler = telegram.ext.CommandHandler("start", start)
 telegram_dispatcher.add_handler(start_handler)
 
 def get_prices(bot,update):
+    reply = ''
     # TODO: Fetch BTC price from FILE, assign it to btc_price
     # TODO: Fetch BCH price from FILE, assign it to bch_price
     # TODO: Fetch LTC price from FILE, assign it to ltc_price
     # TODO: Fetch ETH price from FILE, assign it to eth_price
     # TODO: Fetch DASH price from FILE, assign it to dash_price
-    with(open 'prices.csv', newline='') as prices:
-    reader = csv.reader(prices, delimiter=',')
-    for row in reader:
-        print(', '.join(row))
-    pass
-
-
-    bot.send_message(chat_id=update.message.chat_id, text=btc_price+"\n"+
-                     ltc_price+"\n"+eth_price+"\n"+bch_price+"\n"+dash_price)
+    with open('prices.csv', 'r') as prices:
+        reader = csv.DictReader(prices)
+        for line in reader:
+            reply+='1 #'+line["cryptocurrency"].upper()+' = '+line["price_eur"]+' EUR & '+line["price_usd"]+' USD \n'
+            last_update=line["date"]
+        reply+='Last update performed at '+last_update
+    bot.send_message(chat_id=update.message.chat_id, text=reply)
 get_prices_handler = telegram.ext.CommandHandler("get_prices", get_prices)
 telegram_dispatcher.add_handler(get_prices_handler)
 
