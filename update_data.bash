99ab6e56cf98fc08fdc299dd820f15a2ce0da100
L-Cafe <L-Cafe@users.noreply.github.com>
2017-12-28 18:02:15 +0900

    lol

--- update_data.bash
+++ update_data.bash
@@ -1,10 +1,5 @@
 #!/usr/bin env bash
 
-# Telegram Bot token
-export telegram_token="<YOUR TELEGRAM TOKEN HERE>"
-# Telegram admin ID
-export telegram_admin_id="<YOUR ID HERE>"
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
