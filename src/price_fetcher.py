fb413da92f41c098072ed1088a028677afa31726
L-Cafe <L-Cafe@users.noreply.github.com>
2018-01-15 19:53:42 +0100

    EXTRA THICC

--- src/price_fetcher.py
+++ src/price_fetcher.py
@@ -37,50 +37,32 @@ def _get_prices_():
 def _parse_prices_(json, from_currency, to_currency):
     ret_val = None
     try:
-        ret_val = Decimal(float(json[1]['price_eur'])).quantize(Decimal(".01"))
         i = 0
         while json[i]['symbol'] != from_currency.upper():
             i += 1
-        ret_val = Decimal(float(json[i]['price_'+to_currency])).quantize(Decimal(".01"))
+        ret_val = Decimal(float(json[i]['price_'+to_currency.lower()])).quantize(Decimal(".01"))
     except Exception as err:
         raise Exception(f'Error when parsing prices: {err}')
     finally:
         return ret_val
 
-#TODO: FIXME: There is an app-breaking bug where the prices appear to be the same.
 # public functions
 def fetch_prices():
     """Returns a dictionary with the first element being the currency and the second element being
     the currency to convert to.
     """
-    ret_val = None
+    ret_val = {}
+
     try:
         prices = _get_prices_()
 
-        btc_eur = _parse_prices_(prices, BTC, EUR)
-        btc_usd = _parse_prices_(prices, BTC, USD)
-        bch_eur = _parse_prices_(prices, BCH, EUR)
-        bch_usd = _parse_prices_(prices, BCH, USD)
-        ltc_eur = _parse_prices_(prices, LTC, EUR)
-        ltc_usd = _parse_prices_(prices, LTC, USD)
-        eth_eur = _parse_prices_(prices, ETH, EUR)
-        eth_usd = _parse_prices_(prices, ETH, USD)
-        dash_eur = _parse_prices_(prices, DASH, EUR)
-        dash_usd = _parse_prices_(prices, DASH, USD)
-        xmr_eur = _parse_prices_(prices, XMR, EUR)
-        xmr_usd = _parse_prices_(prices, XMR, USD)
-        xrp_eur = _parse_prices_(prices, XRP, EUR)
-        xrp_usd = _parse_prices_(prices, XRP, USD)
-
-        ret_val = {"BTC": {EUR: btc_eur, USD: btc_usd},
-                   "BCH": {EUR: bch_eur, USD: bch_usd},
-                   "LTC": {EUR: ltc_eur, USD: ltc_usd},
-                   "ETH": {EUR: eth_eur, USD: eth_usd},
-                   "DASH":{EUR: dash_eur, USD: dash_usd},
-                   "XMR": {EUR: xmr_eur, USD: xmr_usd},
-                   "XRP": {EUR: xrp_eur, USD: xrp_usd}}
+        for crypto in CRYPTO_CURRENCIES:
+            for fiat in FIAT_CURRENCIES:
+                if crypto not in ret_val:
+                    ret_val[crypto] = {}
+                ret_val[crypto][fiat] = _parse_prices_(prices, crypto, fiat)
     except Exception as err:
-        print(f'Error when fetching data: {err}') # TODO: Probably should move this into a proper error logging method
+        print(f'Error when fetching data: {err}')
     finally:
         return ret_val
 
